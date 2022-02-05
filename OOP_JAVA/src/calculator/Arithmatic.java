6c3d31d3002b96154f19a6b9b5416ec821a4c200
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-02-05 21:31:34 +0600

    constructor

--- OOP_JAVA/src/calculator/Arithmatic.java
+++ OOP_JAVA/src/calculator/Arithmatic.java
@@ -5,10 +5,20 @@ public class Arithmatic {
 	// instance variable
 	public int add_result;
 	
+	
+	/*
+	 * A constructor in Java is a special method that is used to initialize objects. The constructor is called when an object of a class is created. It can be used to set initial values for object attributes:
+	 */
+	public Arithmatic() { // no need to clll, class name
+		System.out.println("Arithmatic constructor call");
+	}
+	
+	// method
 	public int add(int a, int b) {
 		return add_result =  a + b; // why this
 	}
 	
+	// method
 	public void add_result() {
 		System.out.println(add_result);
 	}
