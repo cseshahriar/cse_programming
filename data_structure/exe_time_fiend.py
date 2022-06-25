6977a5d3d03080c2bfe58290febdab8d749ef420
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-06-25 14:45:35 +0600

    script time diff

--- data_structure/exe_time_fiend.py
+++ data_structure/exe_time_fiend.py
@@ -2,15 +2,11 @@ import time
 
 
 def dominant(n: int):
-    # result = 0
-    # for i in range(n):
-    #     result += i
-    # return result       # 0.04031062126159668
-    return sum(range(n))  # 0.010517120361328125
+    return sum(range(n))  # upper limit false so n + 1 is current value
 
 
 start_time = time.time()
-print(dominant(1000000))  # 499999500000
+print(dominant(100))
 end_time = time.time()
 
 print("Script execution time", end_time - start_time)
